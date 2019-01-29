6aeb3e8c0e5588349d5f26a0ac3a1098d03df225
Mike Lopez <mikeglopez.91@gmail.com>
2019-01-29 00:34:20 -0600

    Add date for testing purposes

--- persistence.js
+++ persistence.js
@@ -1,6 +1,7 @@
 /*
 From: Codewars
 By: joh_pot
+Date: 01/29/19
 
 Write a function, persistence, that takes in a positive parameter num and returns its multiplicative persistence, which is the number of times you must multiply the digits in num until you reach a single digit.
 

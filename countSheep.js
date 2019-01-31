687e7fdcdaded377827e36cc894ce96d6262533e
Mike <mikeglopez.91@gmail.com>
2019-01-30 23:36:52 -0600

    Complete countSheep

---
+++ countSheep.js
@@ -0,0 +1,18 @@
+/*
+From: CodeWars
+By: joshra
+Date: 01/30/19
+
+If you can't sleep, just count sheep!!
+
+Task:
+Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+*/
+
+var countSheep = function (num){
+  var output = '';
+  for (var i = 1; i <= num; i++) {
+    output += `${i} sheep...`;
+  }
+  return output;
+}
